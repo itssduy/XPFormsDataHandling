895e8a48e6c99c173edd5db571c2418a2e473e33
Duy <duypersonal512@gmail.com>
2025-09-30 04:21:06 -0500

    Create basic CRUD endpoints & validation

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,8 +3,15 @@ const { Router } = require("express");
 const usersController = require("../controllers/usersController");
 const usersRouter = Router();
 
+// Basic CRUD endpoints
 usersRouter.get("/", usersController.usersListGet);
+
 usersRouter.get("/create", usersController.usersCreateGet);
 usersRouter.post("/create", usersController.usersCreatePost);
+
+usersRouter.get("/:id/update", usersController.usersUpdateGet);
+usersRouter.post("/:id/update", usersController.usersUpdatePost);
+
+usersRouter.post("/:id/delete", usersController.usersDeletePost);
 
 module.exports = usersRouter;
